9c26866216b69062de7edc1af2c7e034ac35eee8
Matous Kucera <mat.kuc@volny.cz>
2014-05-30 13:53:13 +0200

    Test for message throughput

--- bin/test-messages.sh
+++ bin/test-messages.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# Test which creates many connections to the broker and each connection (client) sents small amount of messages
+# Test which creates certain amount of connections to the broker each connection (client) sends more messages
 
 ip=11.22.33.44
 port=1883
 jar=../target/mqtt-test-suite-0.3-SNAPSHOT-jar-with-dependencies.jar
 topic="test/topic"
 message="Test message"
-msgCount=10
-clientCount=(
+msgCount=(
     1 10 50 100 200 500 1000 2000 5000 10000 20000 30000 40000 50000
     1 10 50 100 200 500 1000 2000 5000 10000 20000 30000 40000 50000
     1 10 50 100 200 500 1000 2000 5000 10000 20000 30000 40000 50000
 )
+clientCount=500
 
 control_c()
 # run if user hits control-c
@@ -24,9 +24,9 @@ control_c()
 # trap keyboard interrupt (control-c)
 trap control_c SIGINT
 
-for i in ${clientCount[@]}
+for i in ${msgCount[@]}
 do
         java -Xmx1g -cp ${jar} "com.tado.mqtt.suite.cli.Publishers" \
-            -h "tcp://$ip:$port" -t "$topic" -m "$message" --msg-count ${msgCount} -pc --csv \
-            --client-count ${i}
+            -h "tcp://$ip:$port" -t "$topic" -m "$message" --msg-count ${i} -pc --csv \
+            --client-count ${clientCount}
 done
